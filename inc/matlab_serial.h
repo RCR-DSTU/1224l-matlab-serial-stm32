2f0bdeeb4ae99466595d463b51ad82bc09e70acf
Caska <alexandrnazarov32@gmail.com>
2024-04-30 14:44:12 +0300

    30/04/24 (#3) Добавление макроса для перегрузки функций

--- inc/matlab_serial.h
+++ inc/matlab_serial.h
@@ -13,70 +13,83 @@ typedef struct {
 	UART_HandleTypeDef *interface;
 	uint8_t *message_buffer_tx;
 	uint8_t *message_buffer_rx;
-	uint8_t *start_symbol_buffer;
-	uint8_t *end_symbol_buffer;
+	uint8_t start_symbol_buffer;
+	uint16_t end_symbol_buffer;
 }matlab_serial_t;
 
-#define matlab_serial_init(obj, value, start_symbol, end_symbol) _Generic((value),\
-		float *: matlab_serial_init_default((matlab_serial_t *)obj, (float *)value, start_symbol, end_symbol),\
-		default: (void)0U)
+#ifndef ROBOTIC_LIBRARY_STATIC
+#include "stdlib.h"
+#include "string.h"
+#ifndef NDEBUG
+#define VALUE_CAN_BE_NULL(expression)	assert(expression != NULL);
+#else /* NDEBUG */
+#define VALUE_CAN_BE_NULL(expression)	if(expression == NULL) return;
+#endif /* NDEBUG */
 
-void matlab_serial_init_default(
+#define matlab_serial_init(obj, value, start_symbol, end_symbol) _Generic((value),\
+		float *: matlab_serial_init_f32((matlab_serial_t *)obj, (float *)value, start_symbol, end_symbol),\
+		uint8_t *: matlab_serial_init_u8((matlab_serial_t *)obj, (uint8_t *)value, start_symbol, end_symbol),\
+		uint16_t *: matlab_serial_init_u16((matlab_serial_t *)obj, (uint16_t *)value, start_symbol, end_symbol),\
+		uint32_t *: matlab_serial_init_u32((matlab_serial_t *)obj, (uint32_t *)value, start_symbol, end_symbol),\
+		default: (void)0U)
+#endif
+
+void matlab_serial_init_f32(
 	matlab_serial_t *object,
 	float *value_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
-void matlab_serial_init_default_hardware_in_the_loop(
+void matlab_serial_init_f32_hardware_in_the_loop(
 	matlab_serial_t *object,
 	float *value_tx_pointer,
 	float *value_rx_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
-void matlab_serial_init_float_array(
+void matlab_serial_init_f32_array(
 	matlab_serial_t *object,
 	float *array_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
 void matlab_serial_init_u8(
 	matlab_serial_t *object,
 	uint8_t *value_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
 void matlab_serial_init_u8_array(
 	matlab_serial_t *object,
 	uint8_t *array_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
 void matlab_serial_init_u16(
 	matlab_serial_t *object,
 	uint16_t *value_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
 void matlab_serial_init_u16_array(
 	matlab_serial_t *object,
 	uint16_t *array_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
 void matlab_serial_init_u32(
 	matlab_serial_t *object,
 	uint32_t *value_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
 void matlab_serial_init_u32_array(
 	matlab_serial_t *object,
 	uint32_t *array_pointer,
 	uint8_t start_symbol,
-	uint8_t end_symbol);
+	uint16_t end_symbol);
 
-void matlab_serial_finish(void);
+void matlab_serial_finish(matlab_serial_t *object);
 
 
 #endif /* USE_HAL_DRIVER */
--- src/matlab_serial.c
+++ src/matlab_serial.c
@@ -1,1 +1,49 @@
 #include "matlab_serial.h"
+
+void matlab_serial_init_default(
+	matlab_serial_t *object,
+	float *value_pointer)
+{
+	VALUE_CAN_BE_NULL(object);
+	VALUE_CAN_BE_NULL(value_pointer);
+
+	// start byte + float 4 byte + end 2 byte
+	uint8_t *msg = (uint8_t *)malloc(sizeof(float) + 3);
+#ifdef _STRING_H_
+	memset(&msg, 0x00, sizeof(msg));
+#endif
+
+	object->message_buffer_rx = msg;
+	object->start_symbol_buffer = 0x3A;
+	object->end_symbol_buffer = 0x0D0A;
+}
+
+void matlab_serial_init_f32(
+	matlab_serial_t *object,
+	float *value_pointer,
+	uint8_t start_symbol,
+	uint16_t end_symbol)
+{
+	VALUE_CAN_BE_NULL(object);
+	VALUE_CAN_BE_NULL(value_pointer);
+
+	// start byte + float 4 byte + end 2 byte
+	uint8_t *msg = (uint8_t *)malloc(sizeof(float) + 3);
+#ifdef _STRING_H_
+	memset(&msg, 0x00, sizeof(msg));
+#endif
+	object->message_buffer_rx = msg;
+	object->start_symbol_buffer = start_symbol;
+	object->end_symbol_buffer = end_symbol;
+}
+
+void matlab_serial_finish(matlab_serial_t *object)
+{
+	VALUE_CAN_BE_NULL(object);
+
+	free(object->message_buffer_rx);
+	free(object->message_buffer_tx);
+	object->end_symbol_buffer = (uint16_t)0x00;
+	object->start_symbol_buffer = 0x00;
+	object->interface = NULL;
+}
