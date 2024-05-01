a1abf8f0c4dbf5983dfb61caaa56dd0b272ea64b
Caska <alexandrnazarov32@gmail.com>
2024-05-01 20:14:41 +0300

    01/05/24 (#4) Добавление функций для передачи float, uint8_t переменных Э

--- inc/matlab_serial.h
+++ inc/matlab_serial.h
@@ -15,81 +15,232 @@ typedef struct {
 	uint8_t *message_buffer_rx;
 	uint8_t start_symbol_buffer;
 	uint16_t end_symbol_buffer;
+	/*!
+	 * 	int8_t, uint8_t, int16_t, uint16_t, uint32_t, int32_t, float
+	 * 	@todo char, double
+	 */
+	unsigned int data_operating_type: 3; // 0-7
+	unsigned int RESERVED: 5;
 }matlab_serial_t;
 
 #ifndef ROBOTIC_LIBRARY_STATIC
 #include "stdlib.h"
 #include "string.h"
+typedef enum {
+	STATUS_OK = 0,
+	OBJECT_NULL = 1,
+	INVALID_DATA_TYPE = 2,
+}status_t;
 #ifndef NDEBUG
 #define VALUE_CAN_BE_NULL(expression)	assert(expression != NULL);
+#define DATA_CAN_BE_NOT_U8(type)		assert(type == 0);
+#define DATA_CAN_BE_NOT_U16(type)		assert(type == 1);
+#define DATA_CAN_BE_NOT_U32(type)		assert(type == 2);
+#define DATA_CAN_BE_NOT_FLOAT(type)		assert(type == 7);
 #else /* NDEBUG */
 #define VALUE_CAN_BE_NULL(expression)	if(expression == NULL) return;
 #endif /* NDEBUG */
 
-#define matlab_serial_init(obj, value, start_symbol, end_symbol) _Generic((value),\
-		float *: matlab_serial_init_f32((matlab_serial_t *)obj, (float *)value, start_symbol, end_symbol),\
-		uint8_t *: matlab_serial_init_u8((matlab_serial_t *)obj, (uint8_t *)value, start_symbol, end_symbol),\
-		uint16_t *: matlab_serial_init_u16((matlab_serial_t *)obj, (uint16_t *)value, start_symbol, end_symbol),\
-		uint32_t *: matlab_serial_init_u32((matlab_serial_t *)obj, (uint32_t *)value, start_symbol, end_symbol),\
+#define matlab_serial_init(obj, pointer, start_symbol, end_symbol) _Generic((pointer),\
+		float *: matlab_serial_init_f32((matlab_serial_t *)obj, (float *)pointer, start_symbol, end_symbol),\
+		uint8_t *: matlab_serial_init_u8((matlab_serial_t *)obj, (uint8_t *)pointer, start_symbol, end_symbol),\
+		uint16_t *: matlab_serial_init_u16((matlab_serial_t *)obj, (uint16_t *)pointer, start_symbol, end_symbol),\
+		uint32_t *: matlab_serial_init_u32((matlab_serial_t *)obj, (uint32_t *)pointer, start_symbol, end_symbol),\
+		int8_t *: matlab_serial_init_i8((matlab_serial_t *)obj, (int8_t *)pointer, start_symbol, end_symbol),\
+		int16_t *: matlab_serial_init_i16((matlab_serial_t *)obj, (int16_t *)pointer, start_symbol, end_symbol),\
+		int32_t *: matlab_serial_init_i32((matlab_serial_t *)obj, (int32_t *)pointer, start_symbol, end_symbol),\
 		default: (void)0U)
+
+#define matlab_serial_send(obj, data, timeout) _Generic((data),\
+		float *: matlab_serial_send_f32((matlab_serial_t *)obj, (float *)data), timeout),\
+		default: (void)0U
 #endif
 
-void matlab_serial_init_f32(
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_init_f32(
 	matlab_serial_t *object,
 	float *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
 
-void matlab_serial_init_f32_hardware_in_the_loop(
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_send_f32(
 	matlab_serial_t *object,
-	float *value_tx_pointer,
-	float *value_rx_pointer,
-	uint8_t start_symbol,
-	uint16_t end_symbol);
+	float *data,
+	uint32_t timeout);
 
-void matlab_serial_init_f32_array(
-	matlab_serial_t *object,
-	float *array_pointer,
-	uint8_t start_symbol,
-	uint16_t end_symbol);
-
-void matlab_serial_init_u8(
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_init_u8(
 	matlab_serial_t *object,
 	uint8_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
 
-void matlab_serial_init_u8_array(
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_send_u8(
 	matlab_serial_t *object,
-	uint8_t *array_pointer,
+	uint8_t *data,
+	uint32_t timeout);
+
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_init_i8(
+	matlab_serial_t *object,
+	int8_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
 
-void matlab_serial_init_u16(
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_send_i8(
+	matlab_serial_t *object,
+	int8_t *data,
+	uint32_t timeout);
+
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_init_u16(
 	matlab_serial_t *object,
 	uint16_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
 
-void matlab_serial_init_u16_array(
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_init_i16(
 	matlab_serial_t *object,
-	uint16_t *array_pointer,
+	int16_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
 
-void matlab_serial_init_u32(
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_init_u32(
 	matlab_serial_t *object,
 	uint32_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
 
-void matlab_serial_init_u32_array(
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_init_i32(
 	matlab_serial_t *object,
-	uint32_t *array_pointer,
+	int32_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
 
-void matlab_serial_finish(matlab_serial_t *object);
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_send_u32(
+	matlab_serial_t *object,
+	float *data,
+	uint32_t timeout);
+
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_send_u16(
+	matlab_serial_t *object,
+	float *data,
+	uint32_t timeout);
+
+/**
+ * @brief This function calculates the sum of two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_finish(matlab_serial_t *object);
 
 
 #endif /* USE_HAL_DRIVER */
--- src/matlab_serial.c
+++ src/matlab_serial.c
@@ -1,24 +1,9 @@
 #include "matlab_serial.h"
 
-void matlab_serial_init_default(
-	matlab_serial_t *object,
-	float *value_pointer)
-{
-	VALUE_CAN_BE_NULL(object);
-	VALUE_CAN_BE_NULL(value_pointer);
-
-	// start byte + float 4 byte + end 2 byte
-	uint8_t *msg = (uint8_t *)malloc(sizeof(float) + 3);
-#ifdef _STRING_H_
-	memset(&msg, 0x00, sizeof(msg));
-#endif
-
-	object->message_buffer_rx = msg;
-	object->start_symbol_buffer = 0x3A;
-	object->end_symbol_buffer = 0x0D0A;
-}
-
-void matlab_serial_init_f32(
+/*!
+ * 	matlab_serial_init_f32
+ */
+status_t matlab_serial_init_f32(
 	matlab_serial_t *object,
 	float *value_pointer,
 	uint8_t start_symbol,
@@ -32,12 +17,78 @@ void matlab_serial_init_f32(
 #ifdef _STRING_H_
 	memset(&msg, 0x00, sizeof(msg));
 #endif
-	object->message_buffer_rx = msg;
+	object->message_buffer_tx = msg;
 	object->start_symbol_buffer = start_symbol;
 	object->end_symbol_buffer = end_symbol;
+	object->data_operating_type = 7;
+
+	return STATUS_OK;
 }
 
-void matlab_serial_finish(matlab_serial_t *object)
+status_t matlab_serial_send_f32(
+	matlab_serial_t *object,
+	float *data,
+	uint32_t timeout)
+{
+	VALUE_CAN_BE_NULL(object);
+	VALUE_CAN_BE_NULL(data);
+	DATA_CAN_BE_NOT_FLOAT(object->data_operating_type);
+
+#ifdef USE_HAL_DRIVER
+	union {
+		float data;
+		uint8_t bytes[4];
+	}convert_float_to_u8;
+
+	convert_float_to_u8.data = *data;
+	HAL_UART_Transmit(object->interface, (uint8_t *)convert_float_to_u8.bytes,
+			sizeof(convert_float_to_u8.bytes), timeout);
+
+#endif /* USE_HAL_DRIVER */
+	return STATUS_OK;
+}
+
+status_t matlab_serial_init_u8(
+	matlab_serial_t *object,
+	uint8_t *value_pointer,
+	uint8_t start_symbol,
+	uint16_t end_symbol)
+{
+	VALUE_CAN_BE_NULL(object);
+	VALUE_CAN_BE_NULL(value_pointer);
+
+	// start byte + uint8_t 1 byte + end 2 byte
+	uint8_t *msg = (uint8_t *)malloc(sizeof(uint8_t) + 3);
+#ifdef _STRING_H_
+	memset(&msg, 0x00, sizeof(msg));
+#endif
+	object->message_buffer_tx = msg;
+	object->start_symbol_buffer = start_symbol;
+	object->end_symbol_buffer = end_symbol;
+	object->data_operating_type = 0;
+
+	return STATUS_OK;
+}
+
+status_t matlab_serial_send_u8(
+	matlab_serial_t *object,
+	uint8_t *data,
+	uint32_t timeout)
+{
+	VALUE_CAN_BE_NULL(object);
+	VALUE_CAN_BE_NULL(data);
+	DATA_CAN_BE_NOT_U8(object->data_operating_type);
+
+#ifdef USE_HAL_DRIVER
+	HAL_UART_Transmit(object->interface, (uint8_t *)data,
+			sizeof(data), timeout);
+#endif /* USE_HAL_DRIVER */
+	return STATUS_OK;
+}
+
+
+
+status_t matlab_serial_finish(matlab_serial_t *object)
 {
 	VALUE_CAN_BE_NULL(object);
 
@@ -46,4 +97,7 @@ void matlab_serial_finish(matlab_serial_t *object)
 	object->end_symbol_buffer = (uint16_t)0x00;
 	object->start_symbol_buffer = 0x00;
 	object->interface = NULL;
+	object->data_operating_type = 0;
+
+	return STATUS_OK;
 }
