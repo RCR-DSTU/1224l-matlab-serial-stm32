0a066ed5157073e19a1718cc2a1fb0e53a3c75fc
Casonka <alexandrnazarovfalloutgame4ch@gmail.com>
2024-05-02 16:44:03 +0300

    02/05/24 (#5) Отладка и первичное тестирование float32 отправки

--- inc/matlab_serial.h
+++ inc/matlab_serial.h
@@ -12,54 +12,74 @@
 typedef struct {
 	UART_HandleTypeDef *interface;
 	uint8_t *message_buffer_tx;
+	uint8_t	buffer_tx_size;
 	uint8_t *message_buffer_rx;
+	uint8_t	buffer_rx_size;
 	uint8_t start_symbol_buffer;
-	uint16_t end_symbol_buffer;
+	uint8_t end_symbol_buffer[2];
+	/* data pointer */
+	void *data_pointer;
 	/*!
 	 * 	int8_t, uint8_t, int16_t, uint16_t, uint32_t, int32_t, float
-	 * 	@todo char, double
+	 * 	@todo char, double, uint64_t, int64_t
 	 */
-	unsigned int data_operating_type: 3; // 0-7
-	unsigned int RESERVED: 5;
+	unsigned int data_operating_type : 3; // 0-7
+	unsigned int structure_lock : 1; // 0-1
+	unsigned int RESERVED : 4;
 }matlab_serial_t;
 
 #ifndef ROBOTIC_LIBRARY_STATIC
 #include "stdlib.h"
 #include "string.h"
 typedef enum {
-	STATUS_OK = 0,
-	OBJECT_NULL = 1,
-	INVALID_DATA_TYPE = 2,
+	STATUS_OK = 0x0,
+	OBJECT_NULL = 0x1,
+	INVALID_DATA_TYPE = 0x2,
+	UNDEFINED_DATA_TYPE = 0x3,
+	END_FUNCTION_WITH_ERROR = 0xF,
 }status_t;
+
 #ifndef NDEBUG
-#define VALUE_CAN_BE_NULL(expression)	assert(expression != NULL);
-#define DATA_CAN_BE_NOT_U8(type)		assert(type == 0);
-#define DATA_CAN_BE_NOT_U16(type)		assert(type == 1);
-#define DATA_CAN_BE_NOT_U32(type)		assert(type == 2);
-#define DATA_CAN_BE_NOT_FLOAT(type)		assert(type == 7);
+#define VALUE_CAN_BE_NULL(expression)		assert(expression != NULL);
+#define DATA_TYPE_CAN_BE_ZERO(type)			assert(type == 0);
+#define DATA_TYPE_CAN_BE_NOT_U8(type)		assert(type == 1);
+#define DATA_TYPE_CAN_BE_NOT_U16(type)		assert(type == 2);
+#define DATA_TYPE_CAN_BE_NOT_U32(type)		assert(type == 3);
+#define DATA_TYPE_CAN_BE_NOT_FLOAT(type)	assert(type == 7);
+#define MALLOC_CAN_RETURN_NULL(result)		assert(result != NULL);
 #else /* NDEBUG */
-#define VALUE_CAN_BE_NULL(expression)	if(expression == NULL) return;
+#define VALUE_CAN_BE_NULL(expression)	if(expression == NULL) return OBJECT_NULL;
 #endif /* NDEBUG */
+#define matlab_serial_init(obj, interface, pointer, start_symbol, end_symbol) _Generic((&pointer[0]),\
+			float *: matlab_serial_init_f32((matlab_serial_t *)obj, interface, (float *)&pointer[0], start_symbol, end_symbol),\
+			uint8_t *: matlab_serial_init_u8((matlab_serial_t *)obj, interface, (uint8_t *)pointer, start_symbol, end_symbol),\
+			uint16_t *: matlab_serial_init_u16((matlab_serial_t *)obj, interface, (uint16_t *)pointer, start_symbol, end_symbol),\
+			uint32_t *: matlab_serial_init_u32((matlab_serial_t *)obj, interface, (uint32_t *)pointer, start_symbol, end_symbol),\
+			int8_t *: matlab_serial_init_i8((matlab_serial_t *)obj, interface, (int8_t *)pointer, start_symbol, end_symbol),\
+			int16_t *: matlab_serial_init_i16((matlab_serial_t *)obj, interface, (int16_t *)pointer, start_symbol, end_symbol),\
+			int32_t *: matlab_serial_init_i32((matlab_serial_t *)obj, interface, (int32_t *)pointer, start_symbol, end_symbol),\
+			default: END_FUNCTION_WITH_ERROR)
 
-#define matlab_serial_init(obj, pointer, start_symbol, end_symbol) _Generic((pointer),\
-		float *: matlab_serial_init_f32((matlab_serial_t *)obj, (float *)pointer, start_symbol, end_symbol),\
-		uint8_t *: matlab_serial_init_u8((matlab_serial_t *)obj, (uint8_t *)pointer, start_symbol, end_symbol),\
-		uint16_t *: matlab_serial_init_u16((matlab_serial_t *)obj, (uint16_t *)pointer, start_symbol, end_symbol),\
-		uint32_t *: matlab_serial_init_u32((matlab_serial_t *)obj, (uint32_t *)pointer, start_symbol, end_symbol),\
-		int8_t *: matlab_serial_init_i8((matlab_serial_t *)obj, (int8_t *)pointer, start_symbol, end_symbol),\
-		int16_t *: matlab_serial_init_i16((matlab_serial_t *)obj, (int16_t *)pointer, start_symbol, end_symbol),\
-		int32_t *: matlab_serial_init_i32((matlab_serial_t *)obj, (int32_t *)pointer, start_symbol, end_symbol),\
-		default: (void)0U)
+#define matlab_serial_init_array(obj, interface, pointer, start_symbol, end_symbol) _Generic((&pointer[0]),\
+			float *: matlab_serial_init_f32_array((matlab_serial_t *)obj, interface, (float *)&pointer[0], (uint8_t)(sizeof(pointer)/4), start_symbol, end_symbol),\
+			uint8_t *: matlab_serial_init_u8_array((matlab_serial_t *)obj, interface, (uint8_t *)pointer, (uint8_t)sizeof(pointer), start_symbol, end_symbol),\
+			uint16_t *: matlab_serial_init_u16_array((matlab_serial_t *)obj, interface, (uint16_t *)pointer, (uint8_t)sizeof(pointer), start_symbol, end_symbol),\
+			uint32_t *: matlab_serial_init_u32_array((matlab_serial_t *)obj, interface, (uint32_t *)pointer, (uint8_t)sizeof(pointer), start_symbol, end_symbol),\
+			int8_t *: matlab_serial_init_i8_array((matlab_serial_t *)obj, interface, (int8_t *)pointer, (uint8_t)sizeof(pointer), start_symbol, end_symbol),\
+			int16_t *: matlab_serial_init_i16_array((matlab_serial_t *)obj, interface, (int16_t *)pointer, (uint8_t)sizeof(pointer), start_symbol, end_symbol),\
+			int32_t *: matlab_serial_init_i32_array((matlab_serial_t *)obj, interface, (int32_t *)pointer, (uint8_t)sizeof(pointer), start_symbol, end_symbol),\
+			default: END_FUNCTION_WITH_ERROR)
 
-#define matlab_serial_send(obj, data, timeout) _Generic((data),\
-		float *: matlab_serial_send_f32((matlab_serial_t *)obj, (float *)data), timeout),\
-		default: (void)0U
 #endif
 
+status_t matlab_serial_send(
+	matlab_serial_t *object,
+	uint32_t timeout);
+
 /**
- * @brief This function calculates the sum of two integers.
+ * @brief This function
  *
- * This function takes two integers as input and returns their sum.
+ * This function
  *
  * @param a The first integer.
  * @param b The second integer.
@@ -67,7 +87,25 @@ typedef enum {
  */
 status_t matlab_serial_init_f32(
 	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
 	float *value_pointer,
+	uint8_t start_symbol,
+	uint16_t end_symbol);
+
+/**
+ * @brief This function
+ *
+ * This function
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
+status_t matlab_serial_init_f32_array(
+	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
+	float *array_pointer,
+	uint8_t array_size,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
 
@@ -82,7 +120,6 @@ status_t matlab_serial_init_f32(
  */
 status_t matlab_serial_send_f32(
 	matlab_serial_t *object,
-	float *data,
 	uint32_t timeout);
 
 /**
@@ -96,6 +133,7 @@ status_t matlab_serial_send_f32(
  */
 status_t matlab_serial_init_u8(
 	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
 	uint8_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
@@ -111,7 +149,6 @@ status_t matlab_serial_init_u8(
  */
 status_t matlab_serial_send_u8(
 	matlab_serial_t *object,
-	uint8_t *data,
 	uint32_t timeout);
 
 /**
@@ -125,6 +162,7 @@ status_t matlab_serial_send_u8(
  */
 status_t matlab_serial_init_i8(
 	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
 	int8_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
@@ -154,6 +192,7 @@ status_t matlab_serial_send_i8(
  */
 status_t matlab_serial_init_u16(
 	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
 	uint16_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
@@ -169,6 +208,7 @@ status_t matlab_serial_init_u16(
  */
 status_t matlab_serial_init_i16(
 	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
 	int16_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
@@ -184,6 +224,7 @@ status_t matlab_serial_init_i16(
  */
 status_t matlab_serial_init_u32(
 	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
 	uint32_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
@@ -199,6 +240,7 @@ status_t matlab_serial_init_u32(
  */
 status_t matlab_serial_init_i32(
 	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
 	int32_t *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol);
@@ -214,7 +256,6 @@ status_t matlab_serial_init_i32(
  */
 status_t matlab_serial_send_u32(
 	matlab_serial_t *object,
-	float *data,
 	uint32_t timeout);
 
 /**
@@ -228,7 +269,6 @@ status_t matlab_serial_send_u32(
  */
 status_t matlab_serial_send_u16(
 	matlab_serial_t *object,
-	float *data,
 	uint32_t timeout);
 
 /**
--- src/matlab_serial.c
+++ src/matlab_serial.c
@@ -1,25 +1,90 @@
 #include "matlab_serial.h"
 
+status_t matlab_serial_send(
+	matlab_serial_t *object,
+	uint32_t timeout)
+{
+	VALUE_CAN_BE_NULL(object);
+	switch(object->data_operating_type)
+	{
+	case 0:
+		return UNDEFINED_DATA_TYPE;
+//	case 1:
+//		return matlab_serial_send_u8(object, timeout);
+//	case 2:
+//		return matlab_serial_send_u16(object, timeout);
+//	case 3:
+//		return matlab_serial_send_u32(object, timeout);
+	case 7:
+		return matlab_serial_send_f32(object, timeout);
+
+	default: return END_FUNCTION_WITH_ERROR;
+	}
+}
+
 /*!
  * 	matlab_serial_init_f32
  */
 status_t matlab_serial_init_f32(
 	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
 	float *value_pointer,
 	uint8_t start_symbol,
 	uint16_t end_symbol)
 {
 	VALUE_CAN_BE_NULL(object);
+	VALUE_CAN_BE_NULL(interface);
 	VALUE_CAN_BE_NULL(value_pointer);
 
 	// start byte + float 4 byte + end 2 byte
 	uint8_t *msg = (uint8_t *)malloc(sizeof(float) + 3);
+	MALLOC_CAN_RETURN_NULL(msg);
 #ifdef _STRING_H_
-	memset(&msg, 0x00, sizeof(msg));
+	memset(&(*msg), 0x00, sizeof(*msg));
 #endif
+	object->interface = interface;
+	object->data_pointer = (void *)value_pointer;
 	object->message_buffer_tx = msg;
+	object->message_buffer_rx = NULL;
+	object->buffer_tx_size = 7;
+	object->buffer_rx_size = 0;
 	object->start_symbol_buffer = start_symbol;
-	object->end_symbol_buffer = end_symbol;
+	object->end_symbol_buffer[0] = (uint8_t)((end_symbol&0xFF00) >> 8);
+	object->end_symbol_buffer[1] = (uint8_t)(end_symbol&0xFF);
+	object->data_operating_type = 7;
+
+	return STATUS_OK;
+}
+
+status_t matlab_serial_init_f32_array(
+	matlab_serial_t *object,
+	UART_HandleTypeDef *interface,
+	float *array_pointer,
+	uint8_t array_size,
+	uint8_t start_symbol,
+	uint16_t end_symbol)
+{
+	VALUE_CAN_BE_NULL(object);
+	VALUE_CAN_BE_NULL(interface);
+	VALUE_CAN_BE_NULL(array_pointer);
+
+	uint8_t byte_size = 3 + (sizeof(float) * array_size);
+
+	// start byte + float 4 byte * sizeof + end 2 byte
+	uint8_t *msg = (uint8_t *)malloc(byte_size);
+	MALLOC_CAN_RETURN_NULL(msg);
+#ifdef _STRING_H_
+	memset(&(*msg), 0x00, byte_size);
+#endif
+	object->interface = interface;
+	object->data_pointer = (void *)array_pointer;
+	object->message_buffer_tx = msg;
+	object->message_buffer_rx = NULL;
+	object->buffer_tx_size = byte_size;
+	object->buffer_rx_size = 0;
+	object->start_symbol_buffer = start_symbol;
+	object->end_symbol_buffer[0] = (uint8_t)((end_symbol&0xFF00) >> 8);
+	object->end_symbol_buffer[1] = (uint8_t)(end_symbol&0xFF);
 	object->data_operating_type = 7;
 
 	return STATUS_OK;
@@ -27,66 +92,42 @@ status_t matlab_serial_init_f32(
 
 status_t matlab_serial_send_f32(
 	matlab_serial_t *object,
-	float *data,
 	uint32_t timeout)
 {
 	VALUE_CAN_BE_NULL(object);
-	VALUE_CAN_BE_NULL(data);
-	DATA_CAN_BE_NOT_FLOAT(object->data_operating_type);
+	DATA_TYPE_CAN_BE_NOT_FLOAT(object->data_operating_type);
+
+	// insert start byte
+	object->message_buffer_tx[0] = object->start_symbol_buffer;
+
+	// insert two end bytes
+	object->message_buffer_tx[object->buffer_tx_size - 2] =
+			object->end_symbol_buffer[0];
+	object->message_buffer_tx[object->buffer_tx_size - 1] =
+			object->end_symbol_buffer[1];
+
+	// calculating float numbers and replace to tx buffer
+	for(int i = 0; i < ((object->buffer_tx_size - 3) / 4); i++)
+	{
+		union {
+			float data;
+			uint8_t bytes[4];
+		}convert_float_to_u8;
+
+		convert_float_to_u8.data = (*(((float *)object->data_pointer) + i));
+
+		for(int j = 0; j < sizeof(convert_float_to_u8.bytes); j++)
+		{
+			object->message_buffer_tx[(i*4)+(j+1)] = convert_float_to_u8.bytes[j];
+		}
+	}
 
 #ifdef USE_HAL_DRIVER
-	union {
-		float data;
-		uint8_t bytes[4];
-	}convert_float_to_u8;
-
-	convert_float_to_u8.data = *data;
-	HAL_UART_Transmit(object->interface, (uint8_t *)convert_float_to_u8.bytes,
-			sizeof(convert_float_to_u8.bytes), timeout);
-
+	HAL_UART_Transmit(object->interface, (uint8_t *)object->message_buffer_tx,
+			object->buffer_tx_size, timeout);
 #endif /* USE_HAL_DRIVER */
 	return STATUS_OK;
 }
-
-status_t matlab_serial_init_u8(
-	matlab_serial_t *object,
-	uint8_t *value_pointer,
-	uint8_t start_symbol,
-	uint16_t end_symbol)
-{
-	VALUE_CAN_BE_NULL(object);
-	VALUE_CAN_BE_NULL(value_pointer);
-
-	// start byte + uint8_t 1 byte + end 2 byte
-	uint8_t *msg = (uint8_t *)malloc(sizeof(uint8_t) + 3);
-#ifdef _STRING_H_
-	memset(&msg, 0x00, sizeof(msg));
-#endif
-	object->message_buffer_tx = msg;
-	object->start_symbol_buffer = start_symbol;
-	object->end_symbol_buffer = end_symbol;
-	object->data_operating_type = 0;
-
-	return STATUS_OK;
-}
-
-status_t matlab_serial_send_u8(
-	matlab_serial_t *object,
-	uint8_t *data,
-	uint32_t timeout)
-{
-	VALUE_CAN_BE_NULL(object);
-	VALUE_CAN_BE_NULL(data);
-	DATA_CAN_BE_NOT_U8(object->data_operating_type);
-
-#ifdef USE_HAL_DRIVER
-	HAL_UART_Transmit(object->interface, (uint8_t *)data,
-			sizeof(data), timeout);
-#endif /* USE_HAL_DRIVER */
-	return STATUS_OK;
-}
-
-
 
 status_t matlab_serial_finish(matlab_serial_t *object)
 {
@@ -94,7 +135,8 @@ status_t matlab_serial_finish(matlab_serial_t *object)
 
 	free(object->message_buffer_rx);
 	free(object->message_buffer_tx);
-	object->end_symbol_buffer = (uint16_t)0x00;
+	object->end_symbol_buffer[0] = (uint8_t)0x00;
+	object->end_symbol_buffer[1] = (uint8_t)0x00;
 	object->start_symbol_buffer = 0x00;
 	object->interface = NULL;
 	object->data_operating_type = 0;
